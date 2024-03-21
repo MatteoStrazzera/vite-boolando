b6b2ad0471a87bab45e9dcd21d64d067375469dc
MatteoStrazzera <mattestrazz@gmail.com>
2024-03-21 15:45:40 +0100

    add html structure divided in three components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<template>
+    <footer class="site-footer">
+        <div class="container d-flex">
+            <div class="company-info">
+                <span><strong>Booleano s.r.l.</strong></span>
+                <ul class="links">
+                    <li><a href="">Informazioni legali</a></li>
+                    <li><a href="">Informativa sulla privacy</a></li>
+                    <li><a href="">Diritto di recesso</a></li>
+                </ul>
+            </div>
+            <div class="socials">
+                <h5>Trovaci anche su</h5>
+                <div class="social_icons">
+                    <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+
+    <header class="site-header">
+        <nav class="container d-flex">
+            <div class="gender">
+                <a href=""><strong>Donna</strong></a>
+                <a href=""><strong>Uomo</strong></a>
+                <a href=""><strong>Bambino</strong></a>
+            </div>
+            <div class="logo">
+                <img width="100" src="/public/images/boolean-logo.png" alt="">
+            </div>
+            <div class="icons">
+                <a href=""><i class="fa-regular fa-user"></i></a>
+                <a href=""><i class="fa-regular fa-heart"></i></a>
+                <a href=""><i class="fa-solid fa-bag-shopping"></i></a>
+            </div>
+        </nav>
+
+    </header>
+
+</template>
+
+<style></style>
